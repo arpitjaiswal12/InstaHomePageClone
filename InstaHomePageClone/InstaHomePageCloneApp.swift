24e140dd7808beca5d1bb1c8015a4f4575ea9c94
Arpit Jaiswal <jaiswalarpit419@gmail.com>
2025-01-24 10:57:27 +0530

    Initial Commit

---
+++ InstaHomePageClone/InstaHomePageCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaHomePageCloneApp.swift
+//  InstaHomePageClone
+//
+//  Created by Arpit Jaiswal on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct InstaHomePageCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
